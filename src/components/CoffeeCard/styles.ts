989798e8b3ea87deeb0dccd808b4fdde7ea54763
Pedro Ferreira <pedrodif@icloud.com>
2022-09-29 19:05:37 -0300

    style: :art: Conversion from px measures to rem

--- src/components/CoffeeCard/styles.ts
+++ src/components/CoffeeCard/styles.ts
@@ -9,23 +9,23 @@ height: 19.375rem;
 display: flex;
 flex-direction: column;
 text-align: center;
-border-top-left-radius: 6px;
-border-top-right-radius: 36px;
-border-bottom-left-radius: 36px;
-border-bottom-right-radius: 6px;
+border-top-left-radius: 0.375rem;
+border-top-right-radius: 2.25rem;
+border-bottom-left-radius: 2.25rem;
+border-bottom-right-radius: 0.375rem;
 background-color: ${(props) => props.theme['card-base']};
 z-index: 0;
 // provisional solution
-margin-top: 50px;
-margin-bottom: 20px;
+margin-top: 3.125rem;
+margin-bottom: 1.25rem;
 box-shadow: 0px 0px 0px 0.5px rgba(0, 0, 0, 0.4);
 `
 
 export const CardImageWrapper = styled.div`
-width: 120px;
-height: 120px;
+width: 7.5rem;
+height: 7.5rem;
 margin: 0 auto;
-margin-top: -30px;
+margin-top: -1.875rem;
 z-index: 1;
 `
 
@@ -36,28 +36,28 @@ margin: 0 auto;
 display: flex;
 justify-content: center;
 align-items: center;
-gap: 6px;
+gap: 0.375rem;
 `
 
 export const Ingredients = styled.p`
 display: flex;
 justify-content: center;
 align-items: center;
-width: 100px;
-height: 25px;
-font-size: 10px;
+width: 6.25rem;
+height: 1.563rem;
+font-size: 0.625rem;
 font-weight: 700;
-border-radius: 100px;
+border-radius: 6.25rem;
 color: ${(props) => props.theme['yellow-dark']};
 background-color: ${(props) => props.theme['yellow-light']};
 `
 
 export const CardTitle = styled.strong`
 font-family: 'Baloo 2';
-font-size: 20px;
+font-size: 1.25rem;
 font-weight: 700;
-line-height: 26px;
-margin-bottom: 10px;
+line-height: 1.625rem;
+margin-bottom: 0.625rem;
 color: ${(props) => props.theme['base-subtitle']};
 `
 
@@ -78,20 +78,20 @@ export const CardShoppingWrapper = styled.div`
 display: flex;
 justify-content: space-between;
 align-items: center;
-width: 215px;
-height: 38px;
+width: 13.438rem;
+height: 2.375rem;
 margin: 0 auto;
-margin-bottom: 25px;
+margin-bottom: 1.563rem;
 `
 
 export const CardAmountWrapper = styled.div`
 display: flex;
 justify-content: space-around;
 align-items: center;
-gap: 3px;
-width: 72px;
-height: 38px;
-border-radius: 6px;
+gap: 0.188rem;
+width: 4.5rem;
+height: 2.375rem;
+border-radius: 0.375rem;
 color: ${(props) => props.theme['base-title']};
 background-color:${(props) => props.theme['base-button']};
 `
@@ -100,20 +100,20 @@ export const CardBillWrapper = styled.p`
 display: flex;
 flex-direction: row;
 align-items: center;
-gap: 2px;
-font-size: 16px;
+gap: 0.125rem;
+font-size: 1rem;
 color:${(props) => props.theme['base-text']};
 `
 export const CardBillValue = styled.span`
-font-size: 25px;
+font-size: 1.563rem;
 font-family: 'Baloo 2';
 
 `
 export const CardShopppingButton = styled.button`
-width: 38px;
-height: 38px;
+width: 2.375rem;
+height: 2.375rem;
 border: none;
-border-radius: 6px;
+border-radius: 0.375rem;
 display: flex;
 justify-content: center;
 align-items: center;
