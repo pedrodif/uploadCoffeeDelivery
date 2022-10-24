6b3c868919a4fd1d83377d26593592ba369ec322
Pedro Ferreira <pedrodif@icloud.com>
2022-10-24 12:22:45 -0300

    feat: :art: deliveryPage creation

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { Home } from './pages/Home'
 import { Checkout } from './pages/Checkout'
+import { Delivery } from './pages/Delivery'
 
 export function Router() {
   return (
@@ -12,6 +13,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/delivery" element={<Delivery />} />
       </Route>
     </Routes>
   )
